2f0ec244441d3eb303d37b1fb5f2330a501017f0
mikedurand <durand.8@osu.edu>
2021-01-15 16:06:35 -0500

    best practice update

--- ReachObservations.py
+++ ReachObservations.py
@@ -11,18 +11,19 @@ from numpy import reshape
 class ReachObservations:    
         
     def __init__(self,D,RiverData):
+        self.D=D
         # assign data from input dictionary
-        self.h=RiverData["h"]        
+        self.h=RiverData["h"]                
         
         # et al.
-        self.GetVectorObs(D)
+        self.GetVectorObs()
     
 
     
-    def GetVectorObs(self,D):    
+    def GetVectorObs(self):    
         
         #%% create resahepd versions of observations
-        self.hv=reshape(self.h, (D.nR*D.nt,1) )
+        self.hv=reshape(self.h, (self.D.nR*self.D.nt,1) )
         # self.Sv=reshape(self.S, (D.nR*D.nt,1) )
         # self.wv=reshape(self.w, (D.nR*D.nt,1) )
 
--- RiverIO.py
+++ RiverIO.py
@@ -12,6 +12,7 @@ class RiverIO:
     def __init__(self,IOtype,fname):
         self.type=IOtype
         self.fname=fname
+        self.data={}
         
         if self.type == 'MetroManTxt':
             self.ReadMetroMan()
@@ -24,54 +25,46 @@ class RiverIO:
         # Read observation file in MetroMan text format        
         fid=open(self.fname,"r")
         infile=fid.readlines()
-        
-        # read domain
-        nR=eval(infile[1])
-        buf=infile[3]; buf=buf.split(); xkm=array(buf,float)
-        buf=infile[5]; buf=buf.split(); L=array(buf,float)
-        nt=eval(infile[7]);
-        buf=infile[9]; buf=buf.split(); t=array([buf],float)
-        
-        dt=reshape(diff(t).T*86400 * ones((1,nR)),(nR*(nt-1),1))
-        
-        #specify variable sizes
-        h=empty(  (nR,nt) ) #water surface elevation (wse), [m]
-        # self.h0=empty( (self.D.nR,1)  ) #initial wse, [m]
-        # self.S=empty(  (self.D.nR,self.D.nt) ) #water surface slope, [-]
-        # self.w=empty(  (self.D.nR,self.D.nt) ) #river top width, [m]
-        # self.hv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of h
-        # self.Sv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of S
-        # self.wv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of w
-        # self.sigh=[] #wse uncertainty standard deviation [m]
-        # self.sigS=[] #slope uncertainty standard deviation [-]
-        # self.sigW=[] #width uncertainty standard deviation [m]
-        # self.dA=empty( (self.D.nR,self.D.nt) ) #cross-sectional area anomaly [m^2]
-        # self.dAv=empty( (self.D.nR*(self.D.nt-1)) ) #reshaped version of dA
-        # self.hmin=empty( (self.D.nR,1)  ) #minimum wse, [m]
-        # self.A0Shift=empty( (self.D.nR,1)  ) #difference in A0 between inversion window and all of timeseries
-        
-        #%% read observations   
-        for i in range(0,nR):
-            buf=infile[i+11]; buf=buf.split(); h[i,:]=array(buf,float)
-        # buf=infile[12+self.D.nR]; buf=buf.split(); self.h0=array([buf],float)
-        # for i in range(0,self.D.nR):
-        #     buf=infile[14+self.D.nR+i]; buf=buf.split(); self.S[i,:]=array(buf,float)/1e5; #convert cm/km -> m/m
-        # for i in range(0,self.D.nR):
-        #     buf=infile[15+self.D.nR*2+i]; buf=buf.split(); self.w[i,:]=array(buf,float)
-        # self.sigS=eval(infile[16+self.D.nR*3])/1e5; #convert cm/km -> m/m
-        # self.sigh=eval(infile[18+self.D.nR*3])/1e2; #convert cm -> m
-        # self.sigw=eval(infile[20+self.D.nR*3] )
-             
-        
-        #%%
         fid.close()   
         
-        # smush into a dictionary
-        self.data={
-            "nR": nR,
-            "xkm": xkm,
-            "L": L, 
-            "nt": nt,
-            "t":t,
-            "dt": dt,
-            "h": h}
+        # read domain
+        self.data["nR"]=eval(infile[1])
+        
+        
+        buf=infile[3]; buf=buf.split(); self.data["xkm"]=array(buf,float)
+        buf=infile[5]; buf=buf.split(); self.data["L"]=array(buf,float)
+        self.data["nt"]=eval(infile[7]);
+        buf=infile[9]; buf=buf.split(); self.data["t"]=array([buf],float)
+        
+        #note: move this line to ReachObservations...
+        self.data["dt"]=reshape(diff(self.data["t"]).T*86400 * ones((1,self.data["nR"])),(self.data["nR"]*(self.data["nt"]-1),1))
+        
+        # #specify variable sizes
+        self.data["h"]=empty(  (self.data["nR"],self.data["nt"]) ) #water surface elevation (wse), [m]
+        # self.h0=empty( (self.D.nR,1)  ) #initial wse, [m]
+        # # self.S=empty(  (self.D.nR,self.D.nt) ) #water surface slope, [-]
+        # # self.w=empty(  (self.D.nR,self.D.nt) ) #river top width, [m]
+        # # self.hv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of h
+        # # self.Sv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of S
+        # # self.wv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of w
+        # # self.sigh=[] #wse uncertainty standard deviation [m]
+        # # self.sigS=[] #slope uncertainty standard deviation [-]
+        # # self.sigW=[] #width uncertainty standard deviation [m]
+        # # self.dA=empty( (self.D.nR,self.D.nt) ) #cross-sectional area anomaly [m^2]
+        # # self.dAv=empty( (self.D.nR*(self.D.nt-1)) ) #reshaped version of dA
+        # # self.hmin=empty( (self.D.nR,1)  ) #minimum wse, [m]
+        # # self.A0Shift=empty( (self.D.nR,1)  ) #difference in A0 between inversion window and all of timeseries
+        
+        #%% read observations   
+        for i in range(0,self.data["nR"]):
+            buf=infile[i+11]; buf=buf.split(); self.data["h"][i,:]=array(buf,float)
+        # # buf=infile[12+self.D.nR]; buf=buf.split(); self.h0=array([buf],float)
+        # # for i in range(0,self.D.nR):
+        # #     buf=infile[14+self.D.nR+i]; buf=buf.split(); self.S[i,:]=array(buf,float)/1e5; #convert cm/km -> m/m
+        # # for i in range(0,self.D.nR):
+        # #     buf=infile[15+self.D.nR*2+i]; buf=buf.split(); self.w[i,:]=array(buf,float)
+        # # self.sigS=eval(infile[16+self.D.nR*3])/1e5; #convert cm/km -> m/m
+        # # self.sigh=eval(infile[18+self.D.nR*3])/1e2; #convert cm -> m
+        # # self.sigw=eval(infile[20+self.D.nR*3] )
+             
+        
