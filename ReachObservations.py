df994671ddb4c5dae5e5862b57f0c07698f0f0b7
mikedurand <durand.8@osu.edu>
2021-01-15 16:22:32 -0500

    First functional commit
    This now includes the basic design for a better way to use classes to work with SWOT and other remote sensing observations for river discharge applications. Based on input from Nikki

--- ReachObservations.py
+++ ReachObservations.py
@@ -11,9 +11,17 @@ from numpy import reshape
 class ReachObservations:    
         
     def __init__(self,D,RiverData):
+        
         self.D=D
+        
         # assign data from input dictionary
-        self.h=RiverData["h"]                
+        self.h=RiverData["h"]           
+        self.w=RiverData["w"]
+        self.S=RiverData["S"]
+        self.h0=RiverData["h0"]
+        self.sigh=RiverData["sigh"]
+        self.sigw=RiverData["sigw"]
+        self.sigS=RiverData["sigS"]
         
         # et al.
         self.GetVectorObs()
@@ -24,7 +32,7 @@ class ReachObservations:
         
         #%% create resahepd versions of observations
         self.hv=reshape(self.h, (self.D.nR*self.D.nt,1) )
-        # self.Sv=reshape(self.S, (D.nR*D.nt,1) )
-        # self.wv=reshape(self.w, (D.nR*D.nt,1) )
+        self.Sv=reshape(self.S, (self.D.nR*self.D.nt,1) )
+        self.wv=reshape(self.w, (self.D.nR*self.D.nt,1) )
 
   
--- RiverIO.py
+++ RiverIO.py
@@ -41,30 +41,25 @@ class RiverIO:
         
         # #specify variable sizes
         self.data["h"]=empty(  (self.data["nR"],self.data["nt"]) ) #water surface elevation (wse), [m]
-        # self.h0=empty( (self.D.nR,1)  ) #initial wse, [m]
-        # # self.S=empty(  (self.D.nR,self.D.nt) ) #water surface slope, [-]
-        # # self.w=empty(  (self.D.nR,self.D.nt) ) #river top width, [m]
-        # # self.hv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of h
-        # # self.Sv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of S
-        # # self.wv=empty( (self.D.nR*self.D.nt,1) ) #reshaped version of w
-        # # self.sigh=[] #wse uncertainty standard deviation [m]
-        # # self.sigS=[] #slope uncertainty standard deviation [-]
-        # # self.sigW=[] #width uncertainty standard deviation [m]
-        # # self.dA=empty( (self.D.nR,self.D.nt) ) #cross-sectional area anomaly [m^2]
-        # # self.dAv=empty( (self.D.nR*(self.D.nt-1)) ) #reshaped version of dA
-        # # self.hmin=empty( (self.D.nR,1)  ) #minimum wse, [m]
-        # # self.A0Shift=empty( (self.D.nR,1)  ) #difference in A0 between inversion window and all of timeseries
+        self.data["h0"]=empty( (self.data["nR"],1)  ) #initial wse, [m]
+        self.data["S"]=empty(  (self.data["nR"],self.data["nt"]) ) #water surface slope, [-]
+        self.data["w"]=empty(  (self.data["nR"],self.data["nt"]) ) #river top width, [m]     
+        self.data["sigh"]=[] #wse uncertainty standard deviation [m]
+        self.data["sigS"]=[] #slope uncertainty standard deviation [-]
+        self.data["sigW"]=[] #width uncertainty standard deviation [m]
         
         #%% read observations   
         for i in range(0,self.data["nR"]):
             buf=infile[i+11]; buf=buf.split(); self.data["h"][i,:]=array(buf,float)
-        # # buf=infile[12+self.D.nR]; buf=buf.split(); self.h0=array([buf],float)
-        # # for i in range(0,self.D.nR):
-        # #     buf=infile[14+self.D.nR+i]; buf=buf.split(); self.S[i,:]=array(buf,float)/1e5; #convert cm/km -> m/m
-        # # for i in range(0,self.D.nR):
-        # #     buf=infile[15+self.D.nR*2+i]; buf=buf.split(); self.w[i,:]=array(buf,float)
-        # # self.sigS=eval(infile[16+self.D.nR*3])/1e5; #convert cm/km -> m/m
-        # # self.sigh=eval(infile[18+self.D.nR*3])/1e2; #convert cm -> m
-        # # self.sigw=eval(infile[20+self.D.nR*3] )
+        
+        buf=infile[12+self.data["nR"]]; buf=buf.split(); self.data["h0"]=array(buf,float)
+        
+        for i in range(0,self.data["nR"]):
+            buf=infile[14+self.data["nR"]+i]; buf=buf.split(); self.data["S"][i,:]=array(buf,float)/1e5; #convert cm/km -> m/m
+        for i in range(0,self.data["nR"]):
+            buf=infile[15+self.data["nR"]*2+i]; buf=buf.split(); self.data["w"][i,:]=array(buf,float)
+        self.data["sigS"]=eval(infile[16+self.data["nR"]*3])/1e5; #convert cm/km -> m/m
+        self.data["sigh"]=eval(infile[18+self.data["nR"]*3])/1e2; #convert cm -> m
+        self.data["sigw"]=eval(infile[20+self.data["nR"]*3] )
              
         
